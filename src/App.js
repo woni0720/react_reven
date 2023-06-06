b6d907fb9b51e903e0cf1e8b4bfe647b281fba25
MachoBoy <jiiwon0@gmail.com>
2023-06-06 21:22:23 +0900

    App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './main';
 
+// This is App component
 const App = () => {
     return (
         <div className='App'>
