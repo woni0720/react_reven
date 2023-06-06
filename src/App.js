a01d09d25f0961303be45dff6d896d5daad63086
MachoBoy <jiiwon0@gmail.com>
2023-06-06 21:18:10 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './main';
 
 const App = () => {
-  return (
-    <div className="App">
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Main />}>
-              </Route>
-          </Routes>
-        </BrowserRouter>
-    </div>
-  )
-}
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Main />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
